983b283f465fa7c59abd7c14fab6b88e2bae8143
kohei789 <noguchi.kohei789@gmail.com>
2022-04-03 20:57:26 +0900

    add example code

--- examples/col/collection_examples.go
+++ examples/col/collection_examples.go
@@ -1,23 +1,24 @@
-# go-collections
+package main
 
-functional library for slice
+import (
+	"errors"
+	"fmt"
 
-## Install
+	"github.com/koh789/go-collections/pkg/col"
+)
 
-```
-go get -u github.com/koh789/go-collections
-```
+func main() {
+	exampleMap()
+	exampleMapE()
+	exampleFlatMap()
+	exampleMapWithIndex()
+	exampleFilter()
+	exampleUniq()
+	exampleGroupBy()
+	exampleGroupByUniq()
+	exampleChunk()
+}
 
-## Requirements
-
-go collections library requires Go version `>=1.18`
-
-
-## Usage
-
-### Map
-
-```go:サンプルコード
 func exampleMap() {
 	inputs := []int{1, 2, 3}
 	results := col.Map(inputs, func(num int) string {
@@ -26,12 +27,7 @@ func exampleMap() {
 	fmt.Printf("map input slice  :%v\n", inputs)
 	fmt.Printf("map results      :%v\n", results)
 }
-//map input slice  :[1 2 3]
-//map results      :[result_1 result_2 result_3]
-```
-### MapE
 
-```go:サンプルコード
 func exampleMapE() {
 	errEven := errors.New("even error")
 	inputs := []int{1, 3, 5, 6, 7, 11}
@@ -45,14 +41,7 @@ func exampleMapE() {
 	fmt.Printf("mapE results       :%v\n", results)
 	fmt.Printf("mapE error         :%v\n", err)
 }
-//mapE input slice   :[1 3 5 6 7 11]
-//mapE results       :[]
-//mapE error         :even error
-```
 
-### FlatMap
-
-```go:サンプルコード
 func exampleFlatMap() {
 	type user struct {
 		pets []string
@@ -62,15 +51,7 @@ func exampleFlatMap() {
 	fmt.Printf("flatMap input slice   :%+v\n", inputs)
 	fmt.Printf("flatMap results       :%v\n", results)
 }
-//flatMap input slice   :[{pets:[dog cat]} {pets:[dog pig]} {pets:[dog]}]
-//flatMap results       :[dog cat dog pig dog]
-```
 
-
-
-### MapWithIndex
-
-```go:サンプルコード
 func exampleMapWithIndex() {
 	type user struct {
 		name string
@@ -80,13 +61,7 @@ func exampleMapWithIndex() {
 	fmt.Printf("mapWithIndex input slice   :%+v\n", users)
 	fmt.Printf("mapWithIndex results       :%v\n", results)
 }
-//mapWithIndex input slice   :[{name:AAA} {name:BBB}]
-//mapWithIndex results       :[0_AAA 1_BBB]
-```
 
-### Filter
-
-```go:サンプルコード
 func exampleFilter() {
 	inputs := []int{1, 2, 3, 4, 5}
 	results := col.Filter(inputs, func(num int) bool {
@@ -95,29 +70,13 @@ func exampleFilter() {
 	fmt.Printf("filter input slice   :%+v\n", inputs)
 	fmt.Printf("filter results       :%v\n", results)
 }
-//filter input slice   :[1 2 3 4 5]
-//filter results       :[2 4]
 
-```
-
-### Uniq
-
-```go:サンプルコード
 func exampleUniq() {
 	inputs := []string{"a", "b", "b", "c", "c", "c"}
 	results := col.Uniq(inputs)
 	fmt.Printf("filter input slice   :%+v\n", inputs)
 	fmt.Printf("filter results       :%v\n", results)
 }
-//filter input slice   :[a b b c c c]
-//filter results       :[a b c]
-```
-
-
-### GroupByUniq
-
-
-```go:サンプルコード
 func exampleGroupByUniq() {
 	type user struct {
 		ID   int
@@ -129,13 +88,6 @@ func exampleGroupByUniq() {
 	fmt.Printf("groupByUniq results       :%v\n", results)
 }
 
-//groupByUniq input slice   :[{ID:1 Name:one} {ID:3 Name:three} {ID:10 Name:ten}]
-//groupByUniq results       :map[1:[{1 one}] 3:[{3 three}] 10:[{10 ten}]]
-
-```
-
-### GroupBy
-```go:サンプルコード
 func exampleGroupBy() {
 	type user struct {
 		ID      int
@@ -151,21 +103,10 @@ func exampleGroupBy() {
 	fmt.Printf("groupBy input slice   :%+v\n", inputs)
 	fmt.Printf("groupBy results       :%v\n", results)
 }
-//groupBy input slice   :[{ID:1 Country:JP} {ID:3 Country:JP} {ID:10 Country:US} {ID:50 Country:JP}]
-//groupBy results       :map[JP:[{1 JP} {3 JP} {50 JP}] US:[{10 US}]]
 
-```
-
-### Chunk
-
-```go:サンプルコード
 func exampleChunk() {
 	inputs := []string{"a", "b", "c", "d", "e", "f", "g"}
 	results := col.Chunk(inputs, 3)
 	fmt.Printf("chunk input slice   :%+v\n", inputs)
 	fmt.Printf("chunk results       :%v\n", results)
 }
-//chunk input slice   :[a b c d e f g]
-//chunk results       :[[a b c] [d e f] [g]]
-
-```
